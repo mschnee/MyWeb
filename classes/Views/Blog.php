998482dd9d6b4bfb8d50a5ce4b8c15b7f69feb8c
Matthew Schnee <matthew.schnee@gmail.com>
2012-03-15 18:33:11 -0500

    Adds some History handling and chrome.

---
+++ classes/Views/Blog.php
@@ -0,0 +1,7 @@
+<?php
+
+class Views_Blog implements Interfaces_View {
+    public function html() {
+        return "This will eventually show a blog...";
+    }
+}
---
+++ classes/Views/Projects.php
@@ -0,0 +1,7 @@
+<?php
+
+class Views_Projects implements Interfaces_View {
+    public function html() {
+        return "This will eventually show projects...";
+    }
+}
